bd9613bd7fb9cee3bdb5308474b11951e9ed3919
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-05-28 02:47:04 +0900

    add nofollow noindex

--- src/runner.py
+++ src/runner.py
@@ -7,5 +7,4 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
