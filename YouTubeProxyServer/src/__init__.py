6decd4d3f5eb05cb50db522083cef5bb0bcac682
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-05-30 13:09:52 +0900

    delete unnecessary files

--- YouTubeProxyServer/src/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.secret_key = '%N$E#ug)wh$55Pd'
-app.config.from_object('src.config')
-db = SQLAlchemy(app)
-
-import src.views
--- YouTubeProxyServer/src/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///userinfo.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY='%N$E#ug)wh$55Pd'
