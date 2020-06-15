90cabfdc3527ba8fa5bf5be411c0d13d1f5828d9
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-06-16 05:10:38 +0900

    add .env format

--- debug.py
+++
@@ -1,9 +0,0 @@
-from src import pytube_patch
-import pytube
-#
-# if __name__ == "__main__":
-#     pytube.__main__.apply_descrambler = pytube_patch.apply_descrambler
-#     yt = pytube.YouTube(f"https://www.youtube.com/watch?v=aatr_2MstrI")
-#     with open("streams.txt", "w") as f:
-#         f.write(yt.streams.get_by_itag(248).url)
-#         f.write(yt.streams.get_by_itag(251).url)
